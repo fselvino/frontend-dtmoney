0b14d697fe583a6c5e12892e279fc380696493df
fselvino <fernandoselvino@gmail.com>
2022-05-08 12:27:26 -0300

    EstilizandoModal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,7 @@
 import Modal from "react-modal";
 import {Container } from './styles'
+import closeImg from '../../assets/close.svg'
+
 
 interface NewTransactionModalProps {
     isOpen: boolean
@@ -8,7 +10,7 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({isOpen, onRequestClose}:NewTransactionModalProps){
 
-    return(
+return(
 
  <Modal
 
@@ -17,12 +19,19 @@ onRequestClose={onRequestClose}
 overlayClassName="react-modal-overlay"
 className="react-modal-content"
 >
+    <button 
+    type="button" 
+    onClick={onRequestClose}
+    className="react-modal-close"
+    >
+        <img src={closeImg} alt="Fechar Modal" />
+    </button>
            
            <Container>
                <h2>Cadastrar transação</h2>
-               <input type="text" placeholder="Título" />
+               <input placeholder="Título" />
                <input type="number" placeholder="Valor" />
-               <input type="text" placeholder="Categoria" />
+               <input placeholder="Categoria" />
                <button type="submit">
                    Cadastrar
                </button>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -2,6 +2,57 @@
 import styled from "styled-components";
 
 export const Container =styled.form`
+    h2 {
+        color:var(--text-title);
+        font-size:1.5rem ;
+        margin-bottom: 2rem ;
+    }
+
+    input {
+        width:100% ;
+        padding: 0 1.5rem ;
+        height: 4rem ;
+        border-radius: 0.25rem ;
+
+        border: 1px solid #d7d7d7 ;
+        background: #e7e9ee ;
+
+        font-weight: 400 ;
+        font-size: 1rem ;
+
+       &::placeholder {
+           color: var(--text-title) ;
+       }
+
+       & + input {
+           margin-top: 1rem ;
+       }       
+    }
+
+    button[type="submit"]{
+        width: 100% ;
+        padding:0 1.5rem ;
+        height:4rem ;
+        background:var(--green) ;
+        color:#FFF;
+        border-radius: 0.25rem ;
+        border: 0 ;
+        font-size: 1rem ;
+        margin-top: 1.5rem ;
+        font-weight: 600 ;
+
+        transition: filter 0.2s ;
+        &:hover{
+            filter: brightness(0.9)//quando passar o mouse sobre o botão a luminosidade sera reduzida em 0.9
+        }
+
+    }
+    .react-modal-close {
+        position:absolute ;
+        right:1.5rem ;
+        top:1.5rem ;
+        border:0;
+    }
     
 
 `
